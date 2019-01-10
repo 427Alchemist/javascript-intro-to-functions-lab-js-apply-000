93a1351757bcaf3e45c6e2bf6f324095099c9ce9
Mike Welch <mikewelchchemist@gmail.com>
2019-01-10 01:39:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+ function whisper(string) {
+   return string.toLowerCase()
+ }
+ function logShout(string) {
+   console.log("HELLO")
+ }
+ function logWhisper(string) {
+   console.log("hello")
+ }
+ function sayHiToGrandma(string) {
+   var string = "I can't hear you!"
+   string.toLowerCase() === string
+   console.log(string)
+ }
+ function sayHiToGrandma(string) {
+   var string = "I love you, Grandma."
+   //string.toUpperCase() === string
+   return "I love you, too."
+ }
+function sayHiToGrandma(string) {
+  var string = "I love you, too."
+  //string === "I love you, Grandma."
+  return "YES INDEED!"
+}
